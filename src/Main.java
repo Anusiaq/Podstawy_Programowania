91f24fc621bb9a0517f8967d4b537dced203324e
Anna <anna.zareba@wp.eu>
2021-11-07 13:08:17 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {public static void main(String[] args) {
+    int liczba = 5;
+    int liczba2 = 2;
+    System.out.println("Hello word" + liczba);
+
+
+    System.out.println(liczba % liczba2);
+
+    String name = " Ania ";
+    int age = 25;
+    System.out.println("My name is" + name + " and I have " + age + " years old.");
+}
+}
